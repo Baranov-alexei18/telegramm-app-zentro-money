4779c31a19cfa9aab7b8c54f94d6cec0e8fd573d
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-10-04 22:28:40 +0300

    feat: check works app with telegramm

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,15 @@
 import styles from './styles.module.css';
 
 export const AuthPage = () => {
-  return <div className={styles.wrapper}>AuthPage</div>;
+  const tg = (window as any).Telegram?.WebApp;
+  const user = tg?.initDataUnsafe?.user;
+
+  return (
+    <div className={styles.wrapper}>
+      <span>AuthPage</span>
+      <p>{JSON.stringify(tg, null, 2)}</p>
+      <p>{JSON.stringify(user, null, 2)}</p>
+      <p>{user.id}</p>
+    </div>
+  );
 };
