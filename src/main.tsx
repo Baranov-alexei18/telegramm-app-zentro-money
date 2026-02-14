6e23cff489e3181afe513b0a7800293073b1f785
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2026-02-14 14:11:02 +0300

    feat: add pwa settings for app

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 
 import { App } from './app.tsx';
 
 import './global.css';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('Доступно обновление. Обновить?')) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log('Приложение готово к работе оффлайн');
+  },
+});
 
 createRoot(document.getElementById('root')!).render(<App />);
